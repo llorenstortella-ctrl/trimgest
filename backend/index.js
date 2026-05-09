120c5b98b0e642ca16262e367ef451e74b9e4589
Llorenç Tortella <llorenstortella@gmail.com>
2026-05-09 10:26:45 +0200

    servir frontend desde backend

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const facturasRouter = require('./routes/facturas');
 const generarRouter = require('./routes/generar');
 const authRouter = require('./routes/auth');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,8 +16,10 @@ app.use(express.json());
 app.use('/facturas', facturasRouter);
 app.use('/generar', generarRouter);
 app.use('/auth', authRouter);
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 app.get('/', (req, res) => {
-  res.json({ mensaje: 'TrimGest API funcionando ✓' });
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(PORT, () => {
